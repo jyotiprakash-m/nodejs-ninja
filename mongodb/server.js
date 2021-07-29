7aa6d81a5145d2325cbc5b5e11b3d3773d85216d
jyotiprakash-m <teachmeonline365@gmail.com>
2021-07-29 10:07:17 +0530

    mongodb blog site done

--- mongodb/server.js
+++ mongodb/server.js
@@ -2,10 +2,16 @@ const express = require('express')
 const morgan = require('morgan')
 const mongoose = require('mongoose')
 const app = express();
-const Blog = require('./models/blog')
+const Blog = require('./models/blog');
+const { render } = require('ejs');
 app.set('view engine', 'ejs');
 app.use(morgan('dev'))
 app.use(express.static('public'))
+
+/**
+ * Most use this middleware if you send data from form
+ */
+app.use(express.urlencoded({ extended: true }))
 
 const baseUrl = "mongodb+srv://jyoti:jyoti@mongodb.fyimu.mongodb.net/nodejs-ninja?retryWrites=true&w=majority"
 
@@ -38,6 +44,69 @@ app.get('/blogs', (req, res) => {
             console.log(err)
         })
 })
+
+/**
+ * Create blog in a form
+ */
+app.get('/blogs/create', (req, res) => {
+
+    res.render('create', { title: "Create Blog" });
+
+})
+
+app.post('/blogs', (req, res) => {
+    // console.log(req.body)
+
+    // Here we create a new instance of blog
+    const blog = new Blog(req.body)
+
+    // Save and redirect the blog to the all blogs route
+    blog.save()
+        .then((result) => {
+            res.redirect('/blogs')
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+})
+
+/**
+ * Route parameter
+ * localhost:4000/blogs/:id
+ * localhost:4000/blogs/65465465aa
+ */
+
+app.get('/blogs/:id', (req, res) => {
+    const id = req.params.id;
+    // console.log(id)
+
+    Blog.findById(id)
+        .then((result) => {
+            res.render('details', { blog: result, title: "Blog Details" })
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+})
+
+
+/**
+ * Delete blog
+ */
+
+app.delete('/blogs/:id', (req, res) => {
+    const id = req.params.id;
+    Blog.findByIdAndDelete(id)
+        .then((result) => {
+            res.json({
+                redirect: '/blogs'
+            })
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+})
+
 
 /**
  * Add blog Route
@@ -74,7 +143,7 @@ app.get('/all-blogs', (req, res) => {
 })
 
 /**
- * Add particular blog
+ * Display particular blog route
  */
 
 app.get('/single-blog', (req, res) => {
